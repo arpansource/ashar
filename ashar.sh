ec7418d74e2e4e1070ffcc0baa627b2d41305534
abhishekprajapati1 <ap0661236@gmail.com>
2025-10-23 07:51:57 +0530

    :fire: UPDATE: added download and run function

--- ashar.sh
+++ ashar.sh
@@ -7,14 +7,24 @@ SETUP_GIT="$REPO_URL/git.sh"
 
 LOCAL_ASHAR_DIR="$HOME/ashar"
 
+
+download_and_run() {
+  local script_url=$1
+  local tmp_script
+  tmp_script=$(mktemp)
+  curl -fsSL "$script_url" -o "$tmp_script"
+  bash "$tmp_script"
+  rm -f "$tmp_script"
+}
+
 # upgrade the system
 sudo pacman -Syu --noconfirm
 
 # install base packages
-bash "$BASE_PACKAGES"
+download_and_run "$REPO_URL/base.sh"
 
 # install and setup git
-bash "$SETUP_GIT"
+download_and_run "$REPO_URL/git.sh"
 
 rm -rf "$ASHAR_DIR"
 git clone https://github.com/arpansource/ashar.git "$ASHAR_DIR"
