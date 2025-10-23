e4541c6597ddc0d68b08fe67cc01d915a198ee77
abhishekprajapati1 <ap0661236@gmail.com>
2025-10-23 07:40:37 +0530

    :fire: UPDATE: updated ashar.sh

--- ashar.sh
+++ ashar.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-BASE_PACKAGES="$HOME/ashar/packages/base.sh"
-SETUP_GIT="$HOME/ashar/packages/git.sh"
+REPO_URL="https://raw.githubusercontent.com/arpansource/ashar/main/packages"
+BASE_PACKAGES="$REPO_URL/base.sh"
+SETUP_GIT="$REPO_URL/git.sh"
+
+LOCAL_ASHAR_DIR="$HOME/ashar"
 
 # upgrade the system
 sudo pacman -Syu --noconfirm
@@ -12,3 +15,14 @@ bash "$BASE_PACKAGES"
 
 # install and setup git
 bash "$SETUP_GIT"
+
+if [ -d "$LOCAL_ASHAR_DIR" ]; then
+  echo "Cleaning up..."
+  rm -rf $LOCAL_ASHAR_DIR
+
+  git clone https://github.com/arpansource/ashar.git
+
+  cd ashar
+
+  bash "$LOCAL_ASHAR_DIR/installer.sh"
+fi
